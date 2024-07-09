e06e8dc15b5369cb947fe320aee8991cc9ef3cd3
vinoth2003 <vinothponnusamy2003@gmail.com>
2024-07-09 13:52:27 +0530

    Updated

--- src/components/growingPlant/GrowingPlant.js
+++ src/components/growingPlant/GrowingPlant.js
@@ -2,18 +2,32 @@ import React from "react";
 import "./GrowingPlant.scss";
 
 //Variable
-const PlantStyle = {
-  color: 'purple',
+// const PlantStyle = {
+//   color: 'purple',
+// };
+
+// const PlantName = "Sunflower";
+// const size = 24;
+
+const plants = {
+  name: "Rose",
+  icon: "🌹",
+  size: 30,
 };
 
 //html code, js => {}
 function GrowingPlant() {
   return (
     <div className="Plant">
-      <div className="text">GrowingPlant
-      {/* <div style={PlantStyle}>Hi</div> */}
-      <div className="logo">Hi it is my Plant🌱</div>
-    </div>
+      <div className="text">
+        GrowingPlant
+        {/* <div style={PlantStyle}>Hi</div> */}
+        <div>
+          Hi it is my Plant
+          <span style={{ fontSize: `${plants.size}px` }}>{plants.icon}</span>
+        </div>
+        <div>Plant name is {plants.name}</div>
+      </div>
     </div>
   );
 }
