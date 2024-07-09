03ae199bc02f73e4afda3c0d63862729e042924e
vinoth2003 <vinothponnusamy2003@gmail.com>
2024-07-09 13:18:49 +0530

    first commit

--- src/components/growingPlant/GrowingPlant.js
+++ src/components/growingPlant/GrowingPlant.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./GrowingPlant.scss";
 
+//Variable
+const PlantStyle = {
+  color: 'purple',
+};
+
+//html code, js => {}
 function GrowingPlant() {
   return (
-    <div>GrowingPlant</div>
-  )
+    <div className="Plant">
+      <div className="text">GrowingPlant
+      {/* <div style={PlantStyle}>Hi</div> */}
+      <div className="logo">Hi it is my Plant🌱</div>
+    </div>
+    </div>
+  );
 }
 
-export default GrowingPlant
+export default GrowingPlant;
