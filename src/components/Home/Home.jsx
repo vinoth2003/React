80c8234ce44a943a88e136e48a44aafde4a2a996
vinoth2003 <vinothponnusamy2003@gmail.com>
2024-07-10 13:15:18 +0530

    finally updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,21 +5,31 @@ const plants1 = {
   name: "Rose",
   icon: "🌹",
   size: 40,
+  aboutMe:[
+    "Love pandrava ellam use pannuva  ",
+    "Simble of love  ",
+    "Beautiful type of flower  "
+  ]
 };
 const plants2 = {
   name: "Sunflower",
   icon: "🌻",
   size: 40,
+  aboutMe:[
+    "Ennai ku use pannuvanga  ",
+    "Simble of Oil  ",
+    "Beautiful type of oil"
+  ]
 };
 
 
 function Home() {
   return (
     <div>
-        <h1>Home</h1>
+        <h1>Welcome to GrowingPlant</h1>
         <GrowingPlant plants={plants1}/>
         <GrowingPlant plants={plants2}/>
-        <GrowingPlant/>
+        {/* <GrowingPlant/> */}
     </div>
   )
 }
--- src/components/growingPlant/GrowingPlant.js
+++ src/components/growingPlant/GrowingPlant.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./GrowingPlant.scss";
 
 //Variable
@@ -7,45 +7,59 @@ import "./GrowingPlant.scss";
 // };
 
 
-
-
-
+  const defaultData = {
+    name: "No name provided",
+    icon: "No icon is provided",
+    size: 30,
+  };
 //html code, js => {}
-function GrowingPlant(props) {
-//props - using it has a dynamic change and reusable
+  function GrowingPlant({plants=defaultData}) {
+  // const { plants = defaultData } = props;
+  //destructure and props into plants
+  let { name, icon, size, aboutMe } = plants;
 
-const name = {
-  first:'viki',
-  last:'P'
-}
-//Object destructure
+  const [dynamicSize,setdynamicSize] =   useState(size) //rerender, UI change
+ //useeffect(function,array of dependency) 
+useEffect(()=>{
+  console.log("My code")//does not change
 
-const {first="Default",last,color='Red'} = name
-console.log(first,last,color)
+},[])
+//it has a dependency
+useEffect(()=>{
+  const Interval = setInterval(()=>{
+    // setdynamicSize(dynamicSize+20)
+  },1000)
+  return ()=>clearInterval(Interval)
+},[dynamicSize])
+
+
+  const grow = () =>{
+    setdynamicSize(dynamicSize+20)
+  } 
 
 
 
+  //props - using it has a dynamic change and reusable
 
-const defaultData = {
-  name: "No name provided",
-  icon: "No icon is provided",
-  size: 30,
-};
+  // const name = {
+  //   first:'viki',
+  //   last:'P'
+  // }
+  //Object destructure
 
-const {plants = defaultData} = props
-//destructure and props into plants
+  // const {first="Default",last,color='Red'} = name
+  // console.log(first,last,color)
+
 
   return (
     <div className="Plant">
-      <div className="text">
-        GrowingPlant
-        {/* <div style={PlantStyle}>Hi</div> */}
+        <div>I am a{name},I am growing</div>
         <div>
-          Hi it is my Plant
-          <span style={{ fontSize: `${plants.size}px` }}>{plants.icon}</span>
+          <span style={{ fontSize: `${dynamicSize}px` }}>{icon}</span>
+          {name==="Rose"?<span>⭐</span>: ''}
         </div>
-        <div>Plant name is {plants.name}</div>
-      </div>
+        <button onClick={grow}>Give Water</button>
+        {aboutMe.map((data,index) => <div key={index}>{index+1}.{data}</div>)}
     </div>
   );
 }
